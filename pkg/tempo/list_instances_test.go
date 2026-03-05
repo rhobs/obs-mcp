046de573bbf2aeeecb460e96f3325cfb3a473bcd
Andreas Gerstmayr <agerstmayr@redhat.com>
2026-03-05 16:10:51 +0100

    test instance and tenant handling
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>

--- pkg/tempo/list_instances_test.go
+++ pkg/tempo/list_instances_test.go
@@ -5,72 +5,12 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	dynamicfake "k8s.io/client-go/dynamic/fake"
 
 	"github.com/rhobs/obs-mcp/pkg/tempo/discovery"
 )
 
-var (
-	tempoStackGVR = schema.GroupVersionResource{
-		Group:    "tempo.grafana.com",
-		Version:  "v1alpha1",
-		Resource: "tempostacks",
-	}
-	tempoMonolithicGVR = schema.GroupVersionResource{
-		Group:    "tempo.grafana.com",
-		Version:  "v1alpha1",
-		Resource: "tempomonolithics",
-	}
-)
-
-func newTempoStack(namespace, name string, tenants []string) *unstructured.Unstructured {
-	auth := make([]any, 0, len(tenants))
-	for _, t := range tenants {
-		auth = append(auth, map[string]any{"tenantName": t})
-	}
-
-	obj := &unstructured.Unstructured{}
-	obj.SetGroupVersionKind(schema.GroupVersionKind{
-		Group:   "tempo.grafana.com",
-		Version: "v1alpha1",
-		Kind:    "TempoStack",
-	})
-	obj.SetNamespace(namespace)
-	obj.SetName(name)
-	obj.Object["spec"] = map[string]any{
-		"tenants": map[string]any{
-			"mode":           "openshift",
-			"authentication": auth,
-		},
-	}
-	obj.Object["status"] = map[string]any{
-		"conditions": []any{
-			map[string]any{
-				"type":   "Ready",
-				"status": string(metav1.ConditionTrue),
-			},
-		},
-	}
-	return obj
-}
-
-func newFakeDynamicClient(objects ...runtime.Object) *dynamicfake.FakeDynamicClient {
-	scheme := runtime.NewScheme()
-	return dynamicfake.NewSimpleDynamicClientWithCustomListKinds(scheme,
-		map[schema.GroupVersionResource]string{
-			tempoStackGVR:      "TempoStackList",
-			tempoMonolithicGVR: "TempoMonolithicList",
-		},
-		objects...,
-	)
-}
-
 func TestListInstancesHandler_Success(t *testing.T) {
-	fakeClient := newFakeDynamicClient(
+	fakeClient := newMockK8sClient(
 		newTempoStack("ns1", "stack1", []string{"tenant-a", "tenant-b"}),
 		newTempoStack("ns2", "stack2", []string{"tenant-c"}),
 	)
