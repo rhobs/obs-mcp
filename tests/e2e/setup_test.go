1beb40e3629aa2e7c3ccdf9bbf4fe13470630deb
Andreas Gerstmayr <agerstmayr@redhat.com>
2026-07-22 15:05:32 +0200

    test: ignore failures in creating service account token for e2e tests
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>

--- tests/e2e/setup_test.go
+++ tests/e2e/setup_test.go
@@ -5,6 +5,7 @@ package e2e
 import (
 	"context"
 	"fmt"
+	"log/slog"
 	"os"
 	"os/signal"
 	"syscall"
@@ -38,10 +39,11 @@ func TestMain(m *testing.M) {
 		os.Exit(1)
 	}
 
+	// Token creation fails in OpenShift Prow e2e tests due to limited permissions of the serviceaccount running the tests,
+	// however there we use kubeconfig auth mode on OpenShift and therefore do not need to send an auth header.
 	token, err := createServiceAccountToken(testConfig.Namespace, testConfig.ServiceAccountName)
 	if err != nil {
-		fmt.Printf("Failed to get bearer token: %v\n", err)
-		os.Exit(1)
+		slog.Warn("Failed to create a service account token, continuing without authenticating MCP tool calls", "err", err)
 	}
 
 	mcpClient = NewMCPClient(testConfig.MCPURL, token)
